fc0f5c88242e7920da0d80acba47564fa4270bdf
Lukáš Horák <lukas.horak@ackee.cz>
2022-07-29 08:29:53 +0200

    🔧 Use fork-ts-checker-webpack-plugin

--- template/next.config.js
+++ template/next.config.js
@@ -1,9 +1,20 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+
+    // https://dev.to/chromygabor/add-typescript-type-check-to-next-js-2nbb
+    webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.dev && options.isServer) {
+            config.plugins.push(new ForkTsCheckerWebpackPlugin());
+        }
+
+        return config;
+    },
 };
 
 const sentryWebpackPluginOptions = {
