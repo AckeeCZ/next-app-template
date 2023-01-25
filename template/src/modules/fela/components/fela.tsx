37278f55a415e2566788d082e85fc773c573ca09
Martin Foldyna <martin.foldyna@ackee.cz>
2023-01-25 09:20:06 +0100

    🏷️ Make renderer an optional prop

--- template/src/modules/fela/components/fela.tsx
+++ template/src/modules/fela/components/fela.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { RendererProvider, ThemeProvider } from "react-fela";
-import type { IRenderer } from "fela";
+import React from 'react';
+import { RendererProvider, ThemeProvider } from 'react-fela';
+import type { IRenderer } from 'fela';
 
-import { renderer as fallbackRenderer } from "../services";
-import { theme } from "styles/theme";
+import { theme } from 'styles/theme';
+
+import { renderer as fallbackRenderer } from '../services';
 
 interface FelaProps {
-  children: React.ReactNode;
-  renderer: IRenderer;
+    children: React.ReactNode;
+    renderer?: IRenderer;
 }
 
 function Fela({ renderer, children }: FelaProps) {
-  return (
-    <RendererProvider renderer={renderer || fallbackRenderer} rehydrate>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </RendererProvider>
-  );
+    return (
+        <RendererProvider renderer={renderer || fallbackRenderer} rehydrate>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </RendererProvider>
+    );
 }
 
 export default Fela;
