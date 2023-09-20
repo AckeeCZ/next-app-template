a5570d9fbee9e0d276d312a4ad52b372e70f1fc9
Martin Macura <martin.macura@ackee.cz>
2023-09-20 16:24:20 +0200

    :bug: Fix lokse

---
+++ template/apps/web/lokse.config.js
@@ -0,0 +1,9 @@
+const { createLokseConfig } = require('../../packages/localization/lokse/index.cjs');
+
+const { languages } = require('./src/modules/intl/config/langs.cjs');
+
+// FIXME: test yarn lokse
+module.exports = createLokseConfig({
+    sheets: [],
+    languages: Object.values(languages),
+});
--- template/apps/web/lokse.config.ts
+++
@@ -1,9 +0,0 @@
-import { createLokseConfig } from '@workspace/localization/lokse';
-
-import { languages } from './src/modules/intl/config/langs.mjs';
-
-// FIXME: test yarn lokse
-export default createLokseConfig({
-    sheets: [],
-    languages: Object.values(languages),
-});
