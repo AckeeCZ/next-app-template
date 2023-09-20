d595f52c948b40d6a1c92cd58672ba8f86cac25a
Jiří Čermák <jiri.cermak@ackee.cz>
2023-09-20 10:37:33 +0200

    ♻️ Use generateImportOrder in prettier config

--- template/tooling/prettier/index.mjs
+++ template/tooling/prettier/index.mjs
@@ -1,24 +1,13 @@
-import { config as ackeeConfig } from '@ackee/prettier-config';
+import { config as ackeeConfig, generateImportOrder } from '@ackee/prettier-config';
 
-
-/** @typedef  {import("prettier").Config} PrettierConfig */
-/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
-
-/** @type { PrettierConfig | SortImportsConfig } */
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
 const config = {
     ...ackeeConfig,
-    importOrder: [
-        "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-        "^(next/(.*)$)|^(next$)",
-        "<THIRD_PARTY_MODULES>",
-        "",
-        "^@workspace/(.*)$",
-        "",
-        "^~/",
-        "",
-        "^~/",
-        "^[../]",
-        "^[./]",
-    ],
+    importOrder: generateImportOrder([
+        ['^(react/(.*)$)|^(react$)|^(react-native(.*)$)', '^(next/(.*)$)|^(next$)', '<THIRD_PARTY_MODULES>'],
+        ['^@workspace/(.*)$'],
+        ['^~/'],
+        ['^[../]', '^[./]'],
+    ]),
 };
-export default  config
+export default config;
