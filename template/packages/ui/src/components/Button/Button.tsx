7acf9ffef10aee68aee14b55e456dda21e88af72
Martin Macura <martin.macura@ackee.cz>
2023-09-22 09:29:44 +0200

    :art: Format Button

--- template/packages/ui/src/components/Button/Button.tsx
+++ template/packages/ui/src/components/Button/Button.tsx
@@ -2,6 +2,6 @@ interface ButtonProps {
     disabled?: boolean;
 }
 
-export const Button = ({disabled = false}: ButtonProps) => {
+export const Button = ({ disabled = false }: ButtonProps) => {
     return <button disabled={disabled}>Button</button>;
 };
