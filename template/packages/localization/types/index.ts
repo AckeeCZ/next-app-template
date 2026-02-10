aefc19a27b21301ec97daeeb59293919efdba0b8
David Kocur <david.kocur@ackee.cz>
2026-02-10 17:26:58 +0100

    🎨 Update imports

--- template/packages/localization/types/index.ts
+++ template/packages/localization/types/index.ts
@@ -42,4 +42,4 @@ export function isFormattedMessageValues(obj: any): obj is Parameters<IntlShape[
     return obj && typeof obj === 'object';
 }
 
-export { type IntlShape, type FormattedMessage , IntlProvider } from 'react-intl';
+export * from 'react-intl';
